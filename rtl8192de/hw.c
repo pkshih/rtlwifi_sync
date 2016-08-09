bc10f29ecaf9d3180a3e34965bd164ae47248668
Christian Engelmayer <cengelma@gmx.at>
2016-08-09 21:19:57 +0200

    rtlwifi: rtl8192de: Fix leak in _rtl92de_read_adapter_info()
    In case rtl_get_hwinfo() fails, the function directly returns and leaks the
    already allocated hwinfo memory. Go through the correct exit path.
    
    Signed-off-by: Christian Engelmayer <cengelma@gmx.at>
    Acked-by: Larry Finger <Larry.Finger@lwfinger.net>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>

--- rtl8192de/hw.c
+++ rtl8192de/hw.c
@@ -1760,7 +1760,7 @@ static void _rtl92de_read_adapter_info(struct ieee80211_hw *hw)
 		return;
 
 	if (rtl_get_hwinfo(hw, rtlpriv, HWSET_MAX_SIZE, hwinfo, params))
-		return;
+		goto exit;
 
 	_rtl92de_efuse_update_chip_version(hw);
 	_rtl92de_read_macphymode_and_bandtype(hw, hwinfo);
@@ -1793,6 +1793,7 @@ static void _rtl92de_read_adapter_info(struct ieee80211_hw *hw)
 		break;
 	}
 	rtlefuse->txpwr_fromeprom = true;
+exit:
 	kfree(hwinfo);
 }
 
