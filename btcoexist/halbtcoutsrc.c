b8001789836da8f05d525d19dc73ba642e387038
Ping-Ke Shih <pkshih@realtek.com>
2016-11-30 14:22:28 +0800

    btcoex: Add ant_div_cfg for 8822be, but it still a dummy
    Signed-off-by: Ping-Ke Shih <pkshih@realtek.com>

--- btcoexist/halbtcoutsrc.c
+++ btcoexist/halbtcoutsrc.c
@@ -1337,6 +1337,7 @@ bool exhalbtc_bind_bt_coex_withadapter(void *adapter)
 			     "[BTCoex], Package Type = Non-TFBGA\n");
 
 	btcoexist->board_info.rfe_type = rtl_get_hwpg_rfe_type(rtlpriv);
+	btcoexist->board_info.ant_div_cfg = 0;
 
 	return true;
 }
