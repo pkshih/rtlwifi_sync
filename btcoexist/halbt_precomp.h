3ee12ae9ce12554655692868b3f7695f5aeda2fc
Ping-Ke Shih <pkshih@realtek.com>
2016-11-30 13:39:05 +0800

    btcoex: Add 8822b header files to precomp.h
    Signed-off-by: Ping-Ke Shih <pkshih@realtek.com>

--- btcoexist/halbt_precomp.h
+++ btcoexist/halbt_precomp.h
@@ -48,6 +48,8 @@
 #include "halbtc8723b2ant.h"
 #include "halbtc8821a2ant.h"
 #include "halbtc8821a1ant.h"
+#include "halbtc8822b1ant.h"
+#include "halbtc8822b2ant.h"
 
 #define GetDefaultAdapter(padapter)	padapter
 
